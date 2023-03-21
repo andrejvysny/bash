a31a21461b349da66f072c585dd3148dec524421
Andrej Vyšný <vysnyandrej@gmail.com>
2023-03-22 00:18:14 +0100

    server setup

---
+++ device-setup/server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create user
+username="andrejvysny"
+sudo adduser --shell /bin/bash $username
+sudo chown $username:$username /home/$username
+sudo chmod 700 /home/$username
+sudo su - $username
+# Bash aliases
+
+# SSH
+curl -sSL https://get.andrejvysny.sk/ssh/id_rsa.pub >> /home/andrejvysny/.ssh/authorized_keys
+
+# Install docker
+curl -sSL https://get.docker.com | sh
+sudo groupadd docker
+sudo usermod -aG docker $username
+docker version
