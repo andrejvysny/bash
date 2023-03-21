faf04d1a1239ae1233d5761574785b9f91b4c839
Andrej Vyšný <vysnyandrej@gmail.com>
2023-03-22 00:41:11 +0100

    server setup

--- device-setup/server.sh
+++ device-setup/server.sh
@@ -7,7 +7,7 @@ apt install htop curl wget -y
 # Bash aliases
 
 # SSH
-curl -sSL https://get.andrejvysny.sk/ssh/id_rsa.pub >> /home/andrejvysny/.ssh/authorized_keys
+curl -sSL https://get.andrejvysny.sk/ssh/id_rsa.pub >> /home/$USER/.ssh/authorized_keys
 
 # Install docker
 curl -sSL https://get.docker.com | sh
